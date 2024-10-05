4f8e21088c1384ffd3a7bfae6b77f412f8ff5ac0
niassuh1 <galenwaros@gmail.com>
2024-10-05 15:23:01 +0300

    refactor: separated frontend and backend

--- frontend/lib/core/common/screens/root_screen.dart
+++ frontend/lib/core/common/screens/root_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:greenshift/core/common/controllers/nav_controller.dart';
 import 'package:greenshift/core/common/widgets/bottom_nav_bar.dart';
 import 'package:greenshift/features/home/presentation/screens/home_screen.dart';
+import 'package:greenshift/features/map/presentation/screens/map_screen.dart';
 
 class RootScreen extends ConsumerWidget {
   const RootScreen({super.key});
@@ -15,11 +16,7 @@ class RootScreen extends ConsumerWidget {
         physics: const NeverScrollableScrollPhysics(),
         children: const [
           HomeScreen(),
-          Scaffold(
-            body: Center(
-              child: Text('Map'),
-            ),
-          ),
+          MapScreen(),
           Scaffold(
             body: Center(
               child: Text('Settings'),
